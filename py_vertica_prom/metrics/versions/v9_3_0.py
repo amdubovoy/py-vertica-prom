e2f3a5e96779ebcfe6ed6a98f9fd82ac6ae1ad71
s1yrat <menuridae@ya.ru>
2021-05-25 12:43:42 +0300

    new metrics

--- py_vertica_prom/metrics/versions/v9_3_0.py
+++ py_vertica_prom/metrics/versions/v9_3_0.py
@@ -6,17 +6,15 @@ from py_vertica_prom.metrics.sql_metric import SQLMetric
 class WOSRowCount(SQLMetric):
     sql_query = """
         select  node_name,
-                schema_name || '.' || storage_containers.projection_name as projection_name,
-                total_row_count as vertica_wos_row_count
-        from    v_monitor.storage_containers
+                sum(total_row_count) as vertica_wos_row_count
+        from    storage_containers
         where   storage_type = 'WOS'
-        order by
-                vertica_wos_row_count desc
-        limit   10;
+        group by
+                node_name;
     """
     metric_name = "vertica_wos_row_count"
-    label_names = ["node_name", "projection_name"]
-    metric = Gauge(metric_name, "WOS Projection row count.", label_names)
+    label_names = ["node_name"]
+    metric = Gauge(metric_name, "Current WOS row count.", label_names)
 
     def update_metric(self, new_value, labels):
         self.metric.labels(*labels).set(new_value)
@@ -25,17 +23,32 @@ class WOSRowCount(SQLMetric):
 class WOSUsedBytes(SQLMetric):
     sql_query = """
         select  node_name,
-                schema_name || '.' || storage_containers.projection_name as projection_name,
-                used_bytes as vertica_wos_used_bytes
-        from    v_monitor.storage_containers
+                sum(used_bytes) as vertica_wos_used_bytes
+        from    storage_containers
         where   storage_type = 'WOS'
-        order by
-                vertica_wos_used_bytes desc
-        limit   10;
+        group by
+                node_name;
     """
     metric_name = "vertica_wos_used_bytes"
-    label_names = ["node_name", "projection_name"]
-    metric = Gauge(metric_name, "WOS Projection memory usage in bytes.", label_names)
+    label_names = ["node_name"]
+    metric = Gauge(metric_name, "WOS memory usage in bytes.", label_names)
+
+    def update_metric(self, new_value, labels):
+        self.metric.labels(*labels).set(new_value)
+
+
+class WOSContainerCount(SQLMetric):
+    sql_query = """
+        select  node_name,
+                count(*) as vertica_wos_storage_containers
+        from    storage_containers
+        where   storage_type = 'WOS'
+        group by
+                node_name;
+    """
+    metric_name = "vertica_wos_storage_containers"
+    label_names = ["node_name"]
+    metric = Gauge(metric_name, "WOS sotrage container count.", label_names)
 
     def update_metric(self, new_value, labels):
         self.metric.labels(*labels).set(new_value)
